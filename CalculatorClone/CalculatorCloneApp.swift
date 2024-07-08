0488a62a01a38e96592229f2c44927722f84cd69
Hendra Lijaya <hendralijaya@gmail.com>
2024-07-08 08:52:52 +0700

    Initial Commit

---
+++ CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by hendra on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorClone
+//
+//  Created by hendra on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
